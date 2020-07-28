34763b33280fbd90afab0fca9d1322809f8b796b
Finn <finnterdal@gmail.com>
2020-07-28 15:15:50 -0500

    adds morgan logging

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const path = require('path');
+const morgan = require('morgan');
 const app = express();
+
+app.use(morgan('dev'));
 
 app.use(express.json());
 
